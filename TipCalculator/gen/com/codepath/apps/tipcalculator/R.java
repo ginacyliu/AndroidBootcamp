19bfcff6813127f688de94cb472f8c50b5155c7b
Gina Liu <ginaliu@yahoo-inc.com>
2014-11-10 00:36:19 +0800

    #2 complete some optional stories.

--- TipCalculator/gen/com/codepath/apps/tipcalculator/R.java
+++ TipCalculator/gen/com/codepath/apps/tipcalculator/R.java
@@ -12,23 +12,33 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int ic_person=0x7f020001;
+        public static final int ic_tip=0x7f020002;
     }
     public static final class id {
-        public static final int btnRate1=0x7f060001;
-        public static final int btnRate2=0x7f060002;
-        public static final int btnRate3=0x7f060003;
+        public static final int btnRate1=0x7f060002;
+        public static final int btnRate2=0x7f060003;
+        public static final int btnRate3=0x7f060004;
         public static final int etTotal=0x7f060000;
-        public static final int tvTip=0x7f060004;
+        public static final int linearLayout1=0x7f060001;
+        public static final int sbPerson=0x7f060007;
+        public static final int sbRate=0x7f060005;
+        public static final int tvPerson=0x7f060008;
+        public static final int tvRate=0x7f060006;
+        public static final int tvTip=0x7f06000a;
+        public static final int tvTipLabel=0x7f060009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int person=0x7f040005;
+        public static final int rate=0x7f040004;
         public static final int rate_btn_1=0x7f040001;
         public static final int rate_btn_2=0x7f040002;
         public static final int rate_btn_3=0x7f040003;
-        public static final int tip=0x7f040004;
+        public static final int tip=0x7f040006;
     }
     public static final class style {
         /** 
--- TipCalculator/src/com/codepath/apps/tipcalculator/MainActivity.java
+++ TipCalculator/src/com/codepath/apps/tipcalculator/MainActivity.java
@@ -1,37 +1,115 @@
 package com.codepath.apps.tipcalculator;
 
-import java.text.DecimalFormat;
-
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	EditText etTotal;
-	TextView tvTip;
-	
+    public static final String PREFS_FILE = "MyPrefsFile";
+    public static final String PREFS_KEY_TIP_RATE = "TipCalculatorRate";
+    
+    private EditText etTotal;
+    private TextView tvTip, tvRate, tvPerson;
+    private SeekBar sbRate, sbPerson;
+    private float m_rate = 0.0f;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
+        SharedPreferences settings = getSharedPreferences(PREFS_FILE, 0);
+        m_rate = settings.getFloat(PREFS_KEY_TIP_RATE, 10f);
+        
         etTotal = (EditText) findViewById(R.id.etTotal);
+        etTotal.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                calculateTip();
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+        });
+        
         tvTip = (TextView) findViewById(R.id.tvTip);
-        tvTip.setText(String.format(getResources().getString(R.string.tip), "")); 
+        tvTip.setText(String.format(getResources().getString(R.string.tip), 0.0f)); 
+        
+        tvRate = (TextView) findViewById(R.id.tvRate);
+        sbRate = (SeekBar) findViewById(R.id.sbRate);
+        sbRate.setProgress((int) (m_rate*100/5));
+        tvRate.setText(String.format(getResources().getString(R.string.rate), sbRate.getProgress()*5));
+        sbRate.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar v, int progressValue, boolean fromUser) {
+                tvRate.setText(String.format(getResources().getString(R.string.rate), progressValue*5 ));
+                m_rate = (progressValue == 0) ? 0 : ((float)progressValue)*5/100;
+                calculateTip();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {}
+            
+        });
+        
+        tvPerson = (TextView) findViewById(R.id.tvPerson);
+        sbPerson = (SeekBar) findViewById(R.id.sbPerson);
+        tvPerson.setText(String.format(getResources().getString(R.string.person), (sbPerson.getProgress()+1)));
+        sbPerson.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar v, int progresValue, boolean fromUser) {
+                tvPerson.setText(String.format(getResources().getString(R.string.person), (progresValue+1)));
+                calculateTip();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {}
+            
+        });
     }
     
-    public void calculateTip(View v) {
-    	String tip = "";
-    	if (etTotal.getText().toString().length() > 0) {
-    		float rate = Float.valueOf(v.getTag().toString());
-        	float total = Float.valueOf(etTotal.getText().toString());
-        	tip = new DecimalFormat("$###,###,###.##").format(total*rate);
-    	}
-    	tvTip.setText(String.format(getResources().getString(R.string.tip), tip));
+    @Override
+    protected void onStop() {
+        super.onStop();
+        
+        SharedPreferences settings = getSharedPreferences(PREFS_FILE, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putFloat(PREFS_KEY_TIP_RATE, m_rate);
+        editor.commit();
+    }
+
+    public void changeTip(View v) {
+        m_rate = Float.valueOf(v.getTag().toString());
+        sbRate.setProgress((int) (m_rate*100/5));
+        calculateTip();
+    }
+    
+    public void calculateTip() {
+        float tip = 0.0f;
+        if (etTotal.getText().toString().length() > 0) {
+            float total = Float.valueOf(etTotal.getText().toString());
+            tip = total*m_rate/(sbPerson.getProgress()+1);
+        }
+        tvTip.setText(String.format(getResources().getString(R.string.tip), tip));
     }
 }
